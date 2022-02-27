89c24d8c5d0178f496a4684f8fcefbf8c978ca16
ay0o5h <aayosh553@gmail.com>
2022-02-27 14:52:30 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,17 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-
-createConnection()
-    .then(async (connection) => {
-        app.use(cors());
-        app.use(express.json());
-        app.use("/v1", webv1);
-        app.use("/dash/v1", dashv1);
-        app.use(notFound);
-        app.listen(port, () => console.log(`Running on ${port}`));
-    })
-    .catch((error) => console.log(error));
+try {
+    createConnection()
+        .then(async (connection) => {
+            app.use(cors());
+            app.use(express.json());
+            app.use("/v1", webv1);
+            app.use("/dash/v1", dashv1);
+            app.use(notFound);
+            app.listen(port, () => console.log(`Running on ${port}`));
+        })
+        .catch((error) => console.log(error));
+} catch (error) {
+    console.log(error)
+}
