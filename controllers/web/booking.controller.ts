35a363a83c068ff35cc168998232dbe3e067f6b5
ay0o5h <aayosh553@gmail.com>
2022-03-10 10:33:26 +0300

    booking

--- controllers/web/booking.controller.ts
+++ controllers/web/booking.controller.ts
@@ -1,4 +1,4 @@
-import { errRes, okRes } from "../../utility/util.service";
+import { okRes } from "../../utility/util.service";
 import { Resturant } from './../../src/entity/Resturant';
 
 import moment = require("moment");
@@ -26,9 +26,9 @@ export default class BookingController {
         let o = moment(rest.openDate).format('LT');
         let c = moment(rest.closeDate).format('LT');
         let n = moment().format('LT');
-        if (o === n) return errRes(res, "resturant is open");
-        if (c === n) return errRes(res, "resturant is closed");
-        if (o !== n && c !== n) return errRes(res, "way to go my sun");
+        if (o === n) return okRes(res, "resturant is open");
+        if (c === n) return okRes(res, "resturant is closed");
+        if (o !== n && c !== n) return okRes(res, "way to go my sun");
 
         return okRes(res, { rest, o, c, n });
 
